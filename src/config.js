66273b18fde2138abfdd571ae244422d6a6fec8e
suenz001 <suenz001@yahoo.com.tw>
2026-01-30 23:17:04 +0800

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// 你的 Firebase 設定 (請填入你的真實數據)
+export const firebaseConfig = {
+    apiKey: "AIzaSyANyPbvSa_qXxfzirS8Q8Xp8MlogetLkTc",
+    authDomain: "genesismud-45614.firebaseapp.com",
+    projectId: "genesismud-45614",
+    storageBucket: "genesismud-45614.firebasestorage.app",
+    messagingSenderId: "42378693168",
+    appId: "1:42378693168:web:0bb8af796bd047d9a43bf5",
+    measurementId: "G-6PB3LBMH8T"
+};
